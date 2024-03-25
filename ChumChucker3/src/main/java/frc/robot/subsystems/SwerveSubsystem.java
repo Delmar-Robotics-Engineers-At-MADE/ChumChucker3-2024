633b788e469e70fee3cb14306c5e424c30bacc29
DESKTOP-BKJ7VT6\sysadmin <gallusgallus6@gmail.com>
2024-03-25 12:03:16 -0500

    adding

--- ChumChucker3/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ ChumChucker3/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -34,6 +34,7 @@ public class SwerveSubsystem extends SubsystemBase {
 
   public SwerveSubsystem(File directory)
   {
+    //change
     // Angle conversion factor is 360 / (GEAR RATIO * ENCODER RESOLUTION)
     //  In this case the gear ratio is 12.8 motor revolutions per wheel rotation.
     //  The encoder resolution per motor revolution is 1 per motor revolution.
